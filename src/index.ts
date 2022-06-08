5c1d91f5306a1ed708605f9b98085e3e3b518b12
Alex <bbqsauce108@gmail.com>
2022-06-08 21:22:18 +0300

    new text

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
-    let message = 'Yo Bro! What\'s up! This is my test deploy!';
+    let message = 'Yo Bro! What\'s up! This is my test deploy! Where\'s yours?';
     res.send(message)
 })
 
